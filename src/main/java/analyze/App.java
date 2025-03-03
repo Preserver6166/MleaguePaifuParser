77caaba9b6db1838a7e8c418d11a49193171add4
保存者6166 <michaelchang0719@gmail.com>
2025-03-03 10:57:19 +0800

    Create App.java

---
+++ src/main/java/analyze/App.java
@@ -0,0 +1,28 @@
+package analyze;
+
+import analyze.PaifuAnalyzer;
+import official.OfficialGameInfo;
+import official.OfficialPaifuUtil;
+import tenhou.TenhouPaifu;
+import tenhou.TenhouPaifuUtil;
+
+import java.util.List;
+import java.util.Map;
+
+public class App {
+
+    public static void main (String[] args) {
+        List<OfficialGameInfo> officialGameInfoList = OfficialPaifuUtil.generateOfficialGameInfoList(
+                "L001_S019_0092_01A", "L001_S019_0092_02A");
+        Map<String, TenhouPaifu> tenhouPaifuMap = TenhouPaifuUtil.generateTenhouPaifuMap(officialGameInfoList);
+        PaifuAnalyzer.fun1(officialGameInfoList, tenhouPaifuMap);
+
+//        List<OfficialGameInfo> officialGameInfoList = OfficialPaifuUtil.generateOfficialGameInfoList(
+//                "L001_S001_0001_01A", "L001_S019_0091_02A");
+//        Map<String, TenhouPaifu> tenhouPaifuMap = TenhouPaifuUtil.generateTenhouPaifuMap(officialGameInfoList);
+////        PaifuAnalyzer.fun24_2(officialGameInfoList, tenhouPaifuMap);
+////        PaifuAnalyzer.fun29(officialGameInfoList, tenhouPaifuMap);
+//        PaifuAnalyzer.fun33(officialGameInfoList, tenhouPaifuMap);
+    }
+
+}
